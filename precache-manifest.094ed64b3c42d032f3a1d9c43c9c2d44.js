14e994c7686c3c4d851774a4535931494a36543f
Rithu Eswaramoorthy <rithue2000@gmail.com>
2021-05-18 17:06:42 -0700

    Updates

---
+++ precache-manifest.094ed64b3c42d032f3a1d9c43c9c2d44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f65fbbd82152419855f6a8e730b7dd60",
+    "url": "/housing-app-frontend/index.html"
+  },
+  {
+    "revision": "2350117c28ba671619e0",
+    "url": "/housing-app-frontend/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "4e045a0b469815dceb78",
+    "url": "/housing-app-frontend/static/css/main.db430fef.chunk.css"
+  },
+  {
+    "revision": "2350117c28ba671619e0",
+    "url": "/housing-app-frontend/static/js/2.92ccf0e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/housing-app-frontend/static/js/2.92ccf0e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e045a0b469815dceb78",
+    "url": "/housing-app-frontend/static/js/main.11994ddf.chunk.js"
+  },
+  {
+    "revision": "1f13944846fa8c582db7",
+    "url": "/housing-app-frontend/static/js/runtime-main.a3166bde.js"
+  }
+]);
